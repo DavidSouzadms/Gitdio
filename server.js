bf599cfbb2eef77623f1cbca63c389745a62298d
DavidSouzadms <devid_souza@hotmail.com>
2021-12-12 19:33:02 -0400

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('./src/config/custom-express');
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000...')
+});
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Rota padrão</h1>
+            </body>
+        </html>
+    `);
+});
